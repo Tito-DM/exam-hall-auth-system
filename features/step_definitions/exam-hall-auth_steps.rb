468f088c7e5e4a880daf40da8a10833ae0ee2b6e
tito Domingos <muandatito@gmail.com>
2019-03-18 18:07:10 +0200

    setup cucumber enviroment

---
+++ features/step_definitions/exam-hall-auth_steps.rb
@@ -0,0 +1,116 @@
+Given("{string} has the following recipe exist:") do |email, recipe_table|
+  user = User.find_by_email(email)
+  recipe_table.hashes.each do |recipe|
+    user.recipes.create!(recipe)
+  end
+end
+
+When /^(?:|I )check "([^"]*)"$/ do |field|
+  find('#search_name[value= "name"]').click if field == "name"
+  find('#search_origin[value= "origin"]').set(true) if field == "origin"
+  find('#search_create_by[value= "create_by"]').click if field == "creater"
+end
+
+
+Given("the following ingredients exist:") do |ingredient_table|
+  ingredient_table.hashes.each do |ingredient|
+    recipe = Recipe.find(ingredient[:id])
+    recipe.ingredient.create!(ingredient)
+  end
+end
+
+
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+Then("It should navigate to {string}") do |page_name|
+ visit path_to(page_name)
+end
+
+
+When("I fill in {string} with {string}") do |string, string2|
+  fill_in string,	with: string2
+end
+
+Given("I  click {string}") do |link|
+    click_link 'comment'
+end
+
+When /^I press on (.*)$/ do |button|
+  find('input[value= "Sign Up"]').click  if button == "Sign Up"
+  find('input[value= "Create Recipe"]').click if button == "create recipe"
+  find('input[value= "Add Ingredient"]').click if button == 'Add Ingredient'
+  find('input[value= "Add step"]').click if button == 'Add step'
+
+
+
+  # if button = 'comment'
+  # find('input[id= "comment"]').click
+  # end
+
+  # if button = 'add comment'
+  #   find('input[value= "add comment"]').click
+  # end
+
+
+end
+
+
+
+
+Then("the number of {string} on the database should be {int}") do |string, int|
+ expect(User.count).to eq int if string == "user"
+  expect(Recipe.count).to eq int if string == 'recip'
+   expect(Recipe.count).to eq int if string == 'recipe-del'
+end
+Then("I should see {string}") do |string|
+  page.should have_content(string)
+end
+
+Then("show me the page") do
+  save_and_open_page
+end
+
+Then("It should redirect me to {string}") do |comment_page|
+   visit path_to(comment_page)
+end
+
+Given("the following user exists:") do |table|
+  table.hashes.each do |user|
+    FactoryBot.create(:user, user)
+  end
+end
+
+When("I visit the {string} show page") do |text|
+    user = User.find_by_email(text)
+    visit user_path(user)
+end
+
+Given("I visit the {string} recipe {string} show page") do |string, string2|
+    user = User.find_by_email(string)
+    recipe = user.recipes.find_by(name: string2)
+    visit user_recipe_path(user,recipe)
+end
+
+When("I click on {string}") do |text|
+  click_link_or_button text
+end
+
+Given("{string} has the following ingredient exist:") do |recipe_name, ingredient_table|
+  recipe = Recipe.find_by(name:recipe_name)
+  ingredient_table.hashes.each do |ingredient|
+    recipe.ingredient.create!(ingredient)
+  end
+end
+
+Given("{string} has the following step exist:") do |recipe_name, step_table|
+  recipe = Recipe.find_by(name:recipe_name)
+  step_table.hashes.each do |step|
+    recipe.steps.create!(step)
+  end
+end
+
+Then("I should not see {string}") do |string|
+  page.should have_no_content(string)
+end
---
+++ features/support/paths.rb
@@ -0,0 +1,42 @@
+# Taken from the cucumber-rails project.
+
+module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in web_steps.rb
+  #
+  def path_to(page_name)
+    case page_name
+
+    when /the home\s?page/
+     '/'
+    when /edit recipe\s?page/
+       edit_user_recipe_path(1,1)
+    when /the create new recip page/
+     new_user_recipe_path(User.ids)
+    when /the create new user page/
+      new_user_path
+    when /the details page for 'Calulu'/
+      recipe_path(Recipe.where(name: 'calulu').ids)
+    when /comment page/
+      new_recipe_comment_path(Recipe.where(name: 'calulu').ids)
+
+
+
+
+    # Add more mappings here.
+    # Here is an example that pulls values out of the Regexp:
+    #
+    #   when /^(.*)'s profile page$/i
+    #     user_profile_path(User.find_by_login($1))
+
+    else
+      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+        "Now, go and add a mapping in #{__FILE__}"
+    end
+  end
+end
+
+World(NavigationHelpers)
