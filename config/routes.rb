78efb8a0c79e2eef8abf96297c5a387abb11b9eb
tito Domingos <muandatito@gmail.com>
2019-03-20 19:38:11 +0200

    set nested root between student and subject

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :subjects
-  resources :students
+
+  resources :students do
+      resources :subjects
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'students#index'
