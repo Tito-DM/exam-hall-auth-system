d065f6fcaf007408ae999659385dc94eae093395
tito Domingos <muandatito@gmail.com>
2019-03-20 13:13:32 +0200

    add validation to suject model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,9 @@
 class Subject < ApplicationRecord
   belongs_to :student
+  validates :name, presence: true, length: {minimum:3, maximum:50}
+  validates :code, presence: true
+  validates :final_Mark, presence: true
+  validates :exame_date, presence:true
+  validates :exam_value, presence: true
+  validates :student_id, presence :true
 end
