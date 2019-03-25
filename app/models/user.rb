ff4e9960650a89c38037ddb9ff3bb487a5e70452
tito Domingos <muandatito@gmail.com>
2019-03-25 22:50:27 +0200

    fix the login issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase}
+  before_save { self.username = username.downcase}
   validates :name, presence:true, length: {minimum:3, maximum:50}
   validates :username, presence:true, uniqueness: {case_sensitive: false}
 
