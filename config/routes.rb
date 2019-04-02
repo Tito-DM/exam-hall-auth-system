21a022ab8b8fa078da16c37b21c38fde42e03dda
tito Domingos <muandatito@gmail.com>
2019-04-02 15:41:20 +0200

    change the root page

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'sessions#new'
-  get 'home/verification'
+  root 'home/verification'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
