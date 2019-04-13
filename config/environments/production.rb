f32fdf604e0aba6e72be16b4d8dfc54646c5b968
tito Domingos <muandatito@gmail.com>
2019-04-13 22:50:37 +0200

    test action cable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://shielded-basin-14722.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://shielded-basin-14722.herokuapp.com', 
-    'http://shielded-basin-14722.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'https://shielded-basin-14722.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
